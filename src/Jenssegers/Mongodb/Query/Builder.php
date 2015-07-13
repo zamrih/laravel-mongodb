bbd6923ec41b08876b2bd75fe1ae5a0f638fc9e6
zamrih <zamrih@users.noreply.github.com>
2015-07-13 12:05:12 +0100

    Fix : Adding limit & distinct to generateCacheKey
    Queries with different or no limit could end up sharing the same cache key and the thus the same cache data.
    Same applies to distinct.

--- src/Jenssegers/Mongodb/Query/Builder.php
+++ src/Jenssegers/Mongodb/Query/Builder.php
@@ -275,7 +275,9 @@ class Builder extends QueryBuilder {
             'columns'    => $this->columns,
             'groups'     => $this->groups,
             'orders'     => $this->orders,
+            'distinct'     => $this->distinct,
             'offset'     => $this->offset,
+            'limit'     => $this->limit,
             'aggregate'  => $this->aggregate,
         );
 
