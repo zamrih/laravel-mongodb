932fe2378287ea4b870212ed3620bcbcf1a64268
zamrih <zamrih@users.noreply.github.com>
2015-07-13 12:22:53 +0100

    Fix : Adding distinct to generateCacheKey

--- src/Jenssegers/Mongodb/Query/Builder.php
+++ src/Jenssegers/Mongodb/Query/Builder.php
@@ -316,6 +316,7 @@ class Builder extends BaseBuilder {
             'columns'    => $this->columns,
             'groups'     => $this->groups,
             'orders'     => $this->orders,
+            'distinct'     => $this->distinct
             'offset'     => $this->offset,
             'limit'      => $this->limit,
             'aggregate'  => $this->aggregate,
